15918809cda6a2c79a5ae748b11e9e9176150e9e
Maylilll <642722474@qq.com>
2023-05-27 11:40:17 +0800

    fix:成员列表代码文件名调整

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory,  RouteRecordRaw } from 'vue-router'
 const Live = ()=> import('../components/Live.vue')
-const MemberCard = ()=> import('../view/MemberCard.vue')
+const MemberLiveList = ()=> import('../view/MemberLiveList.vue')
 const KeyInput = ()=> import('../components/KeyInput.vue')
 // const Home = ()=> import('../views/home/home.vue')
 // const About = ()=> import('../views/about/about.vue')
 const routes: Array<RouteRecordRaw> = [
-    {    path: '/',    name: 'memberCard',    component: MemberCard},
+    {    path: '/',    name: 'memberLiveList',    component: MemberLiveList},
     {    path: '/live',    name: 'live',    component: Live},
     {    path: '/keyInput',    name: 'keyInput',    component: KeyInput,  }
     // {    path: '/about',    name: 'about',    component: About  },
--- src/view/MemberLiveList.vue
+++ src/view/MemberLiveList.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { toRefs, watch } from 'vue';
-import { memberCard, reSetReplayDict } from './MemberCard';
+import { memberCard, reSetReplayDict } from './MemberLiveList';
 import { debounce } from '../utils/index';
-import Card from "../components/MemberCard.vue";
+import MemberCard from "../components/MemberCard.vue";
 const { returnRef, handleScroll } = toRefs(memberCard());
 watch(() => returnRef.value.showTopLoading, (newVal) => {
   if(newVal) {
@@ -24,13 +24,13 @@ watch(() => returnRef.value.replayList, (newVal: any[]) => {
   <div @scroll.prevent="debounce(handleScroll)" style="width:100%; height: 100%;">
     <div style="width: 100%">直播</div>
       <div v-masonry gutter="10" :v-if="returnRef.show" class="grid">
-        <Card :live-list = returnRef.liveList></Card>
+        <MemberCard :live-list = returnRef.liveList></MemberCard>
     </div>
     <div style="width: 100%">重播</div>
       <div v-for="dateKey,dateIndex in Object.keys(returnRef.replayDict)"  :key="dateIndex">
         <div style="width: 100%">{{ dateKey }}</div>
         <div v-masonry :v-if="returnRef.recordShow" class="grid">
-          <Card :live-list = returnRef.replayDict[dateKey]></Card>
+          <MemberCard :live-list = returnRef.replayDict[dateKey]></MemberCard>
         </div>
       </div>
   </div>
