03ae42a9202b36a28c3f8121e3dc1c8d21050ea1
maylilll <642722474@qq.com>
2023-06-22 22:03:26 +0800

    fix:macos窗口快捷键失效修复

--- electron/main/win/KeyWin.ts
+++ electron/main/win/KeyWin.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, globalShortcut } from 'electron';
 import { join } from 'node:path';
 export class KeyWin {
   public constructor(win: BrowserWindow) {
@@ -16,6 +16,37 @@ export class KeyWin {
         contextIsolation: false
       }
     });
+
+
+    KeyWin.on('focus', () => {
+      // mac下快捷键失效的问题
+      if (process.platform === 'darwin') {
+        let contents = KeyWin.webContents
+        globalShortcut.register('CommandOrControl+C', () => {
+          console.log('注册复制快捷键成功')
+          contents.copy()
+        })
+        globalShortcut.register('CommandOrControl+V', () => {
+          console.log('注册粘贴快捷键成功')
+          contents.paste()
+        })
+        globalShortcut.register('CommandOrControl+X', () => {
+          console.log('注册剪切快捷键成功')
+          contents.cut()
+        })
+        globalShortcut.register('CommandOrControl+A', () => {
+          console.log('注册全选快捷键成功')
+          contents.selectAll()
+        })
+      }
+    })
+    KeyWin.on('blur', () => {
+      globalShortcut.unregisterAll() // 注销键盘事件
+    })
+    KeyWin.on('close', () => {
+      globalShortcut.unregisterAll() // 注销键盘事件
+    })
+
     KeyWin.menuBarVisible = false;
     const url = process.env.VITE_DEV_SERVER_URL;
     if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
